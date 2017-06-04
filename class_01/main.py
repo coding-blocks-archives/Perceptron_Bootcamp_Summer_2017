e347c19525387a114b3f69524d51d911cc3760e1
shubham1810 <skd.1810@gmail.com>
2017-06-04 13:16:27 +0530

    Code for Day 01:

---
+++ class_01/main.py
@@ -0,0 +1,8 @@
+from pack import my_add
+
+print my_add(12, -10)
+
+if __name__ == '__main__':
+	pass
+
+# https://github.com/shubham1810/ML_Bootcamp/tree/master/day_01
---
+++ class_01/pack.py
@@ -0,0 +1,11 @@
+def my_add(a, b):
+	return a+b
+
+
+class A():
+	def __init__(self):
+		self.x = 12
+
+
+if __name__ == '__main__':
+	print my_add(12, 10)
