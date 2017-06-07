a930b1e735e7a74cea5579e4a4d8e02abc49ac49
shubham1810 <skd.1810@gmail.com>
2017-06-07 11:33:03 +0530

    Face region fix

--- class_03/my_cam.py
+++ class_03/my_cam.py
@@ -6,7 +6,7 @@ facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # print facec
 # font = cv2.FONT_HERSHEY_SIMPLEX
 
-data_collection = True
+data_collection = False
 n = 10
 skip_frame = 10
 data = []
@@ -19,9 +19,12 @@ while True:
 	gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
 	faces = facec.detectMultiScale(gray, 1.3, 5)
 	ix += 1
-
+	fac = None
+	
 	for (x,y,w,h) in faces:
-		fc = fr[x:x+w, y:y+h, :]
+		fc = fr[y:y+h, x:x+w, :]
+		# cv2.imshow('face', fc)
+		# print fc.shape, fr.shape
 		if data_collection:
 			if ix%skip_frame == 0:
 				roi = cv2.resize(fc, (64, 64))
@@ -30,10 +33,11 @@ while True:
 				flag = True
 				break
 		cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+		fac = fc
 	if flag:
 		break
-	cv2.imshow('rgb', fr)
-	cv2.imshow('gray', gray)
+	# cv2.imshow('rgb', fr)
+	# cv2.imshow('gray', gray)
 	if cv2.waitKey(1) == 27:
 		break
 
@@ -42,6 +46,6 @@ print len(data)
 data = np.asarray(data)
 print data.shape
 
-np.save('face_02', data)
+# np.save('face_02', data)
 
 cv2.destroyAllWindows()
